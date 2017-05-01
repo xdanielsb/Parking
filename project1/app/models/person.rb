b942a71702683befb7fe8c142efd9b13716cb4d9
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-01 14:41:42 -0500

    Add fancy validations.

--- project1/app/models/person.rb
+++ project1/app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ApplicationRecord
-  validates :cedula, presence: true
-  validates :name, presence: true
+  validates :cedula, presence: true, uniqueness: true
+  validates :name, presence: true, length: { minimum: 3  }
 end

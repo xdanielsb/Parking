1e739d4b8a800be6281994f23811c8338cc23541
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-01 14:13:42 -0500

    Create register person from view to client.

--- project1/app/controllers/people_controller.rb
+++ project1/app/controllers/people_controller.rb
@@ -7,7 +7,24 @@ class PeopleController < ApplicationController
 
   #Get /people/:id
   def show
-    @people = Person.find(params[:id])
+    @person = Person.find(params[:id])
+  end
+
+  #Get /people/new
+  def new
+    @person  = Person.new
+  end
+
+  #Post Person
+  def create
+    #Create the person
+    @person  = Person.new(cedula: params[:person][:cedula], name: params[:person][:name], age:params[:person][:age], description: params[:person][:description])
+
+    #Save in the database
+    @person.save
+
+    #Display the save register
+    redirect_to  @person
   end
 
 end
